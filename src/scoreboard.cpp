3e28b5a922d722379d35828356c55359faccf068
brooks_yo <yasmine.olivia.brooks@live.mercer.edu>
2018-11-28 02:08:49 -0500

    starting outline of scoreboard idea

---
+++ src/scoreboard.cpp
@@ -0,0 +1,24 @@
+//JUST AN OUTLINE---MIGHT MAKE A CLASS
+
+//clipping window dimensions: clipWindow; bottom three lines of octagon and WINDOW_MAX_Y/X
+//need NUM MISSILES FIRED; NUM ASTEROIDS ON SCREEN; NUM ASTEROIDS HIT #RATIO OF MIS FIRED TO AST DESTROYED
+	//where to get that info from:
+		//game.cpp update all these nnumber from there: missiles.size; asteroids.size; resolveCollisions-->points_in_poly
+
+
+
+//
+void drawString(float x, float y, void *font, const char* string)
+{
+    //define a character to hold chars of string
+    //position text on screen
+    //loop to store current char of string in char defined earlier and add it using glutBitmapCharacter()
+}
+
+void processTextToScreen()
+{
+    //define memory to hold string
+    //put data in string
+    ////drawString()...
+    ////clear up string 
+}
