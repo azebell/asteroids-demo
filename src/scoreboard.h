3ec5b517f6137d46c5e40596f9d86ecdf77e698b
Yasmine Brooks <yasmine.olivia.brooks@live.mercer.edu>
2018-11-30 08:08:46 -0500

    update scoreboard.h

--- src/scoreboard.h
+++ src/scoreboard.h
@@ -1,10 +1,9 @@
-
 #ifndef SCOREBOARD_H
 #define SCOREBOARD_H
 
 void drawString(float x, float y, void *font, const char *text);
-void processTextToScreen();
-void drawScoreboard();
+void processTextToScreen(float x, float y, int number, const char *textFormat);
+void drawScoreboard(int number);
 
 #endif // SCOREBOARD_H
 
