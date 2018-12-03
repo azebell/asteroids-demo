128c7c521be6dc7ce7ae9121a3e56c5b9f8d4d0f
Yasmine Brooks <yasmine.olivia.brooks@live.mercer.edu>
2018-12-03 17:12:25 -0500

    changed layout of scoreboard; fixed spacing; starting to make values update

--- src/scoreboard.cpp
+++ src/scoreboard.cpp
@@ -1,58 +1,45 @@
 //#include "scoreboard.h"
 #include <GL/glut.h>
 #include <stdio.h>
-
-
+#include <vector>
+#include <iostream>
+//
 void drawString(float x, float y, void *font, const char *text) {
-    //define a character to hold chars of string
+//define a character to hold chars of string
     const char *c;
-    //position text on screen
+//position text on screen
     glRasterPos2f(x, y);
-    //loop to store current char of string in char defined earlier and add it using glutBitmapCharacter()
+//loop to store current char of string in char defined earlier and add it using glutBitmapCharacter()
     for(c=text; *c!='\0'; c++) {
-		glutBitmapCharacter(font, *c);
+        glutBitmapCharacter(font, *c);
     }
 }
 
 void processTextToScreen(float x, float y, float number, const char *textFormat) {
-   
-    //define memory to hold string
+//std::cout << "Start process text" << std::endl;
+//define memory to hold string
     char charString[20];
-    //put data in string
+//  put data in string
     sprintf(charString, textFormat, number);
     glColor3ub(0, 255, 128);
-    ////drawString()...
+//drawString()...
     drawString(x, y, GLUT_BITMAP_TIMES_ROMAN_24, charString);
-    ////clear up string 
-}
+//clear up string           std::cout << "Finish process draw" << std::endl;
+}                                                                                  //TODO
 
-//TODO
-//need to format decimal
-//calculate destroyRatio in render method
 
-void drawScoreboard(float number) {
-    
-    float posFactorX = 1000/6; //change to WINDOW_MAX_X and WINDOW_MAX_Y
-    float posFactorY = 1000/10;
-    float secondLineFacX = posFactorX+20;
-    float thirdLineFacX = posFactorX+40;
-    float secondLineFacY = posFactorY-40;
-    float thirdLineFacY = posFactorY-60;
+void drawScoreboard(std::vector<int> values, int height, int width) {
 
-    processTextToScreen(posFactorX, posFactorY, 0, "Missiles");
-    processTextToScreen(secondLineFacX, secondLineFacY, 0, "Fired");
-    processTextToScreen(thirdLineFacX, thirdLineFacY, number, "%d");
+float numMis = (float) values[0];
+float numAs = (float) values[1];
+float posFactorX = ((float)width/8);
+float posFactorY =(float) (height/8)*0.75;
+float ratio =  numAs/ 5.0 * 100;
+processTextToScreen(posFactorX, posFactorY, numMis, "Missiles Fired %.0f");                               
+processTextToScreen(posFactorX, posFactorY-40, numAs, "Asteroids Visible %.0f");
 
-    processTextToScreen(posFactorX*2, posFactorY, 0, "Asteroids");
-    processTextToScreen(secondLineFacX*2, secondLineFacY, 0, "Visible");
-    processTextToScreen(thirdLineFacX*2, thirdLineFacY, number, "%d");
+processTextToScreen(posFactorX*5.50, posFactorY, ratio, "Destroy Ratio %.2f");
 
-    processTextToScreen(posFactorX*4, posFactorY, 0, "Asteroids");
-    processTextToScreen(secondLineFacX*4, secondLineFacY, 0, "Struck");
-    processTextToScreen(thirdLineFacX*4, thirdLineFacY, number, "%d");
-
-    processTextToScreen(posFactorX*5, posFactorY, 0, "Destroy");
-    processTextToScreen(secondLineFacX*5, secondLineFacY, 0, "Ratio");
-    processTextToScreen(thirdLineFacX*5, thirdLineFacY, number, "%d %");
-
+processTextToScreen(posFactorX*5.50, posFactorY-40, 10.0, "Asteroids Struck %.0f");
+ //std::cout<< "P4 Done" << std::endl;
 }
