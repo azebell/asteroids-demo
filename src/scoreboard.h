c0b246d9d6280a96d23c6a066b5424adcf458001
Yasmine Brooks <yasmine.olivia.brooks@live.mercer.edu>
2018-12-03 17:14:01 -0500

    Update scoreboard.h

--- src/scoreboard.h
+++ src/scoreboard.h
@@ -3,7 +3,7 @@
 
 void drawString(float x, float y, void *font, const char *text);
 void processTextToScreen(float x, float y, float number, const char *textFormat);
-void drawScoreboard(float number);
+void drawScoreboard(std::vector<int> values, int height, int width);
 
 #endif // SCOREBOARD_H
 
